0fb036148109f0e2405b99429f18e334c80e9acd
gjy_1992 <gjy_1992@qq.conn>
2019-04-24 00:12:16 +0800

    add some WebGL objects

--- BakeryCanvas/internals/bind.cpp
+++ BakeryCanvas/internals/bind.cpp
@@ -1,14 +1,15 @@
 #include "bind.h"
 
+#define BIND_INTERNAL_FUNC_DIFF(v8name, localname)                                                         \
+  v8::Local<v8::Function> v8_##v8name = v8pp::wrap_function(isolate, #v8name, &bkinternals::localname); \
+  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, #v8name), v8_##v8name);
+
+#define BIND_INTERNAL_FUNC(x) BIND_INTERNAL_FUNC_DIFF(x,x)
+
 void Bind_Internals(v8::Isolate* isolate) {
-  v8::Local<v8::Function> v8_log = v8pp::wrap_function(isolate, "log", &bkinternals::log);
-  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, "log"), v8_log);
-  v8::Local<v8::Function> v8_setTimeout = v8pp::wrap_function(isolate, "setTimeout", &bkinternals::setTimeout);
-  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, "setTimeout"), v8_setTimeout);
-  v8::Local<v8::Function> v8_setInterval = v8pp::wrap_function(isolate, "setInterval", &bkinternals::setInterval);
-  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, "setInterval"), v8_setInterval);
-  v8::Local<v8::Function> v8_clearTimeout = v8pp::wrap_function(isolate, "clearTimeout", &bkinternals::clearTimeout);
-  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, "clearTimeout"), v8_clearTimeout);
-  v8::Local<v8::Function> v8_clearInterval = v8pp::wrap_function(isolate, "clearInterval", &bkinternals::clearInterval);
-  isolate->GetCurrentContext()->Global()->Set(v8::String::NewFromUtf8(isolate, "clearInterval"), v8_clearInterval);
+  BIND_INTERNAL_FUNC(log);
+  BIND_INTERNAL_FUNC(setTimeout);
+  BIND_INTERNAL_FUNC(setInterval);
+  BIND_INTERNAL_FUNC(clearTimeout);
+  BIND_INTERNAL_FUNC(clearInterval);
 }
